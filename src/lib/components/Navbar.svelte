6bc09b2a03c04d7751d2ad504d4f540ee7330df1
Milan Kecman <milan.kecman270@gmail.com>
2026-03-14 20:11:09 +0100

    feat: front page

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,3 @@
+<div class="navbar bg-base-100 shadow-sm flex justify-between p-3">
+  <a href="/" class="btn btn-ghost text-xl">Fullzquat</a>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,17 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import Navbar from "$lib/components/Navbar.svelte";
+</script>
+
+<Navbar />
+
+<div class="hero bg-base-200 min-h-screen">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Greetings stranger!</h1>
+      <p class="py-6">
+        This here is a tribute to one and only, Zercher squat.
+      </p>
+      <button class="btn bg-gray-600 hover:bg-gray-800">Get familiar with Zercher squat</button>
+    </div>
+  </div>
+</div>
